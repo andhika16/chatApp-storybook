533645e5416665ad5e4b7c73f5c95ad5363e020c
andhika16 <moh.andhika16@gmail.com>
2020-06-19 21:31:04 +0700

    belajar file system pada node js

--- app.js
+++ app.js
@@ -1,57 +1,65 @@
-// fungsi node js dibawah ini dibawah ini mengikuti tutorial di w3school
-
+// // kode dibawah ini untuk parsing URL
 // const http = require('http');
-// const dt = require('./logger');
+// const url = require('url');
 
 // http.createServer((req, res) => {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write(testServer());
-//     res.end();
-// }).listen(8080);
+//     res.writeHead(200, { 'Content-Type': 'text-html' });
+//     const q = url.parse(req.url, true).query;
+//     const txt = q.year + " " + q.month;
+//     res.end(txt);
+// }).listen(8080)
 
-// console.log('listening on port ....');
-
-// 
-
-// const http = require('http');
-
-// http.createServer((req, res) => {
-
-//     res.write('hello world');
-//     res.end();
-
-// }).listen(8080);
+// console.log('menjalankan server...8080');
 
 
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write('Hello World!');
-//     res.end();
-// }).listen(8080);
-
-// Read the Query String
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write(req.url);
-//     res.end();
-// }).listen(8080);
+// Node js file system from w3school
 
 
-
-// kode dibawah ini untuk parsing URL
 const http = require('http');
-const url = require('url');
+const fs = require('fs');
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text-html' });
-    const q = url.parse(req.url, true).query;
-    const txt = q.year + '' + q.month;
-    res.end(txt);
-}).listen(8080)
+// http.createServer((req, res) => {
+
+//     fs.readFile('./demoFile1.html', (err, data) => {
+//         res.writeHead(200, { 'Content-Type': 'Text/html' });
+//         res.write(data);
+//         return res.end();
+//     })
+
+// }).listen(8080);
 
 
+// const fs = require('fs');
+
+fs.appendFile('newFile1.txt', 'Hello content', (err) => {
+    if (err) throw err;
+    console.log('saved!');
+});
 
 
+fs.open('newFile2.txt', 'w', (err, file) => {
+    if (err) throw err;
+    console.log('Saved!');
+});
+let txt = 'halo nama saya andhika saya dari ponorogo saya seorang web developer dan impian saya menjadi founder dari profil desa.com';
+
+
+fs.writeFile('newFile2.txt', txt, err => {
+    if (err) throw err;
+    console.log('Saved!');
+
+});
+
+fs.appendFile('newFile2.txt', txt, (err) => {
+    if (err) throw err;
+    console.log('Saved!');
+
+})
+
+
+fs.unlink('newFile2.txt', (err) => {
+    if (err) throw err;
+    console.log('File Deleted !');
+
+})
 
