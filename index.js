3bd4e3a143c49e9d79698e27ee801fefd8bcb849
andhika16 <moh.andhika16@gmail.com>
2020-06-23 13:41:50 +0700

    handling delete request

--- index.js
+++ index.js
@@ -85,6 +85,16 @@ function validateCourse(course) {
     return Joi.validate(course, schema);
 }
 
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with given Id is not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+
+})
 const port = process.env.port || 3000
 app.listen(port, () => console.log(`listening on port ${port}`))
 
