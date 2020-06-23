d2d0360454f1a4af1f181a444dc333f839592deb
andhika16 <moh.andhika16@gmail.com>
2020-06-23 11:38:21 +0700

    handling get request

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 
 
+const course = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+]
+
+
+
 
 
 app.get('/', (req, res) => {
@@ -10,14 +18,18 @@ app.get('/', (req, res) => {
 
 
 
-app.get('/api/courses/:id', (req, res) => {
+app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.query);
+app.get('/api/courses/:id', (req, res) => {
+    const courses = course.find(c => c.id === parseInt(req.params.id));
+    if (!courses) res.status(404).send('404 ID not Found');
+    res.send(courses);
 })
+
+
 
 const port = process.env.port || 3000
 app.listen(port, () => console.log(`listening on port ${port}`))
