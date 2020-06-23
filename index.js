f173e9157e97ab32bd3652b87d39c54a7ecfd292
andhika16 <moh.andhika16@gmail.com>
2020-06-23 12:23:27 +0700

    input validation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
 
 
 const courses = [
@@ -29,7 +31,22 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(courses);
 })
 
+
 app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    const result = Joi.validate(req.body, schema);
+
+
+    // this code below is manual error handling.
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
