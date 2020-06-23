0e53a3a92292943b698b88a1567dc846b26d74f3
andhika16 <moh.andhika16@gmail.com>
2020-06-23 07:16:21 +0700

    melanjutkan node js milik with mosh

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!!')
+})
+
+
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);
+})
+
+const port = process.env.port || 3000
+app.listen(port, () => console.log(`listening on port ${port}`))
+
