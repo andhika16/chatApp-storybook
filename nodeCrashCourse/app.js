63b1cbdc494aefd3586c52f44a62597e9463ca42
andhika16 <moh.andhika16@gmail.com>
2020-07-04 15:41:11 +0700

    mongoDb

--- nodeCrashCourse/app.js
+++ nodeCrashCourse/app.js
@@ -1,44 +1,55 @@
-"use strict";
+'use strict';
 
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-
-app.listen(3000, () => {
-    console.log('server running on port...');
-})
+const mongoose = require('mongoose');
+const Blog = require('./module/blog');
 
 
-// app.use(morgan('dev'));
-app.use(express.static('public'));
+// connect to mongo db
+const dbURI = 'mongodb+srv://andhika:dhika.12345@node-tuts.4yfq2.mongodb.net/nodetuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log('server running on port... & connect to db');
+        })
+    })
+    .catch(err => console.log(err))
+
+
+
 
 // registering view engnine
 app.set('view engine', 'ejs');
 
-// listen for request
-app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'yoshi find eggs', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing' },
-        { title: 'Mario defeat browser', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing' },
-        { title: 'jose catch chickens', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing' }
-    ]
+// midlleware & static files
+app.use(morgan('dev'));
+app.use(express.static('public'));
 
-    // res.sendFile('./views/index.html', { root: __dirname });
-    res.render('index', { title: 'Home', blogs })
+
+// routes
+app.get('/', (req, res) => {
+    res.redirect('/blogs')
 });
 
 app.get('/about', (req, res) => {
-    // res.sendFile('./views/about.html', { root: __dirname });
     res.render('about', { title: 'About' })
 });
-
+// blog & routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((results) => {
+            res.render('index', { title: 'All Blogs', blogs: results })
+        })
+        .catch(err => res.send(err))
+})
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create' });
 })
 
 // 404 page
 app.use((req, res) => {
-    // res.status(404).sendFile('./views/404.html', { root: __dirname });
     res.status(404).render('404', { title: '404' })
 });
 
