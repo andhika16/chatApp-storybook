fba631e469195e65afbf5466a8ee6fbedf0dfb20
andhika16 <moh.andhika16@gmail.com>
2020-09-18 14:23:34 +0700

    project done with brad

--- mychatapp/public/js/main.js
+++ mychatapp/public/js/main.js
@@ -1,1 +1,81 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessage = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users')
+// Get username and room from URL
+const {
+  username,
+  room
+} = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
 const socket = io();
+
+// Join chatroom
+socket.emit('joinRoom', {
+  username,
+  room
+});
+
+// Get room and users
+socket.on('roomUsers', ({
+  room,
+  users
+}) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+// message from server
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message)
+  // scroll down
+  chatMessage.scrollTop = chatMessage.scrollHeight
+})
+
+// message submit
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // get message text
+  const msg = e.target.elements.msg.value;
+
+  // emit message to server
+  socket.emit('chatMessage', msg);
+
+  // clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+
+
+})
+
+
+// outputmessage to dom
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<div class="meta">${message.username} <span>${message.time}</span></div>
+  <p class="text">
+    ${message.text}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div)
+
+}
+
+// add room name to DOM
+
+function outputRoomName(room) {
+  roomName.innerHTML = room
+}
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach(user => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+}
--- mychatapp/server.js
+++ mychatapp/server.js
@@ -1,18 +1,73 @@
 const express = require('express');
-const app = express();
 const http = require('http');
 const PORT = 3000 || process.env.PORT
 const path = require('path');
 const socketio = require('socket.io');
+
+const formatMessage = require('./utils/messages');
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+} = require('./utils/users');
+
+const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
+
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'chatBot'
 // run when user connection
 io.on('connection', socket => {
-    console.log('New socket connetion');
+    socket.on('joinRoom', ({
+        username,
+        room
+    }) => {
+        const user = userJoin(socket.id, username, room)
+
+        socket.join(user.room);
+
+        // welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to chardcode!!'));
+
+        // broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(user.username, `${user.username} has joined the chat`));
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    })
+
+
+
+    // listen for chat message
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id)
+        io.to(user.room).emit('message', formatMessage(`${user.username}`, msg));
+    })
+
+    // runs when the client disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username}has left the chat`));
+            // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
+
+
+
+
+    });
 });
 
 server.listen(PORT, () => console.log(`server connection on port ${PORT}`))
